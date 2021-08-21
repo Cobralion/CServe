dc232dbfe9d6dfbd5c66e6abf7f45ac85f8517c2
Maurice Lindner <lindner.maurice@freenet.de>
2021-08-21 16:41:00 +0200

    Setup

---
+++ CServe/src/Test.cpp
@@ -0,0 +1,8 @@
+#include "Test.h"
+
+#include <iostream>
+
+void test()
+{
+	std::cout << "Hello World!" << std::endl;
+}
---
+++ CServeTest/src/CServeTest.cpp
@@ -0,0 +1,9 @@
+
+#include "Test.h"
+
+int main()
+{
+	test();
+	return 0;
+}
+
